ef9f722be1ed544b429d7cd917a2c692ac2721a5
Ferdinand Kleinschroth <ferdinand.kleinschroth@gmx.de>
2022-04-23 12:07:50 +0000

    recent data view

---
+++ models/staging/stg_kl_recent_weather.sql
@@ -0,0 +1,40 @@
+{{ config(materialized='view') }}
+
+with weather_recent as 
+(
+    select *,
+        row_number() over(partition by STATIONS_ID, MESS_DATUM) as row_number
+        from {{ source('staging', 'kl_recent_partitioned_table')}}
+)
+select
+    -- identifiers
+    {{ dbt_utils.surrogate_key(['STATIONS_ID', 'MESS_DATUM']) }} as mid,
+    STATIONS_ID as station_id,
+    -- observation date
+    --parse_date(cast(MESS_DATUM as string), "%Y%m%d") as mdate
+    parse_date("%Y%m%d", cast(MESS_DATUM as string)) as mdate,
+
+    -- measurements
+    FX as wind_max,
+    FM as wind_mean, 
+    RSK as precip_total,
+    RSKF as precip_category,
+    SDK as sunshine_duration,
+    SHK_TAG as snow_height,
+    NM as cloud_cover,
+    VPM as vapor_pressure,
+    TMK as temperature_2m_mean,
+    UPM as relative_humidity,
+    TXK as temperature_2m_max,
+    TNK as temperature_2m_min,
+    TGK as temperature_5cm_min
+
+from weather_recent
+
+-- select * from {{ source('staging', 'kl_recent_partitioned_table') }}
+
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
